dfaecd0111e401a976d15534bd4a88810034ca38
Cameron Gray <cameron@camerongray.me>
2017-07-05 22:49:52 +0100

    Added all tables to models file, still need to add relationships

--- TeamLocker_Server/models.py
+++ TeamLocker_Server/models.py
@@ -15,6 +15,7 @@ def init(connection_string):
     engine = create_engine(connection_string)
     Base.metadata.bind(engine)
 
+
 class User(Base):
     __tablename__ = "users"
 
@@ -24,3 +25,38 @@ class User(Base):
     encrypted_private_key = Column(Binary)
     encrypted_private_key_nonce = Column(Binary)
     public_key = Column(Binary)
+
+
+class Permission(Base):
+    __tablename__ = "permissions"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    folder_id = Column(Integer, ForeignKey("folders.id"))
+    read = Column(Boolean)
+    write = Column(Boolean)
+
+
+class EncryptedAccountDataItem(Base):
+    __tablename__ = "encrypted_account_data"
+
+    id = Column(Integer, primary_key=True)
+    account_id = Column(Integer, ForeignKey("accounts.id"))
+    user_id = Column(Integer, ForeignKey("users.id"))
+    permission_id = Column(Integer, ForeignKey("permissions.id"))
+    encrypted_metadata = Column(Binary)
+    encrypted_password = Column(Binary)
+
+
+class Account(Base):
+    __tablename__ = "accounts"
+
+    id = Column(Integer, primary_key=True)
+    folder_id = Column(Integer, ForeignKey("folders.id"))
+
+
+class Folder(Base):
+    __tablename__ = "folders"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
