a99e5cef8e8859667587ce6c55ee659f85291eec
Cameron Gray <cameron@camerongray.me>
2017-07-23 23:36:20 +0100

    Get user method working

--- TeamLocker_Server/server.py
+++ TeamLocker_Server/server.py
@@ -4,6 +4,8 @@ import models
 import config
 import crypto
 from flask import Flask, abort, request
+from protobufs.GetUser_pb2 import GetUserResponse
+from protobufs.MessageComponents_pb2 import OperationResult
 
 app = Flask(__name__)
 
@@ -14,6 +16,7 @@ def check_auth():
         abort(401)
 
     user = models.User.query.filter(models.User.username == request.authorization.username).first()
+    request.authenticated_user = user
 
     if not user:
         # TODO: Hash something here to prevent timing attacks?
@@ -28,7 +31,31 @@ def check_auth():
 
 @app.route("/ping", methods=["GET"])
 def ping():
-    return "Foo"
+    return "pong"
+
+
+@app.route("/users/<user_id>/", methods=["GET"])
+def get_users(user_id):
+    if user_id == "self":
+        user = request.authenticated_user
+    else:
+        user = models.User.get(user_id)
+        if not user:
+            abort(404)  # TODO: Return a protobuf with an error message?
+
+    response = GetUserResponse()
+    response.result.success = True
+    response.user.id = user.id
+    response.user.username = user.username
+    response.user.full_name = user.full_name
+    response.user.auth_key_hash = user.auth_key_hash
+    response.user.encrypted_private_key = user.encrypted_private_key
+    response.user.public_key = user.public_key
+    response.user.kdf_salt = user.kdf_salt
+    response.user.is_admin = user.is_admin
+
+    return response.SerializeToString()
+
 
 if __name__ == "__main__":
     models.init(config.connection_string)
